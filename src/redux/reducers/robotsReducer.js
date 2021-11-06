72c90747a526c0311fa108c1a9316e9411013275
ipice1618 <72157834+ipice1618@users.noreply.github.com>
2021-11-06 18:30:39 +0100

    added robots reducer

---
+++ src/redux/reducers/robotsReducer.js
@@ -0,0 +1,20 @@
+import actionTypes from "../actions/actionTypes";
+
+const robotsReducer = (robots = [], action) => {
+  let newRobots;
+
+  switch (action.type) {
+    case actionTypes.getRobots:
+      newRobots = [...action.robots];
+      break;
+    case actionTypes.createRobot:
+      newRobots = [...robots, action.robot];
+      break;
+    default:
+      newRobots = robots;
+  }
+
+  return newRobots;
+};
+
+export default robotsReducer;
