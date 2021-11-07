05d9f662c3d32e4941e699a50004a2f443f3c922
ipice1618 <72157834+ipice1618@users.noreply.github.com>
2021-11-07 16:33:37 +0100

    added server to card tests

--- src/component/Card.test.js
+++ src/component/Card.test.js
@@ -1,4 +1,5 @@
 import { getRandomRobot } from "../mocks/robotFactory";
+import { server } from "../mocks/server";
 import renderWithProviders from "../utils/testing-utils";
 import Card from "./Card";
 
@@ -6,7 +7,14 @@ describe("Given a Card component,", () => {
   let robot;
   beforeAll(() => {
     robot = getRandomRobot();
+    server.listen();
   });
+
+  afterAll(() => {
+    server.close();
+    jest.resetAllMocks();
+  });
+
   describe("When it renders", () => {
     test("Then it renders!", () => {
       renderWithProviders(<Card robot={robot} />);
