e34374bedfba4cdd6b9e721147c3b562421d7fa8
ipice1618 <72157834+ipice1618@users.noreply.github.com>
2021-11-08 19:36:31 +0100

    added logout thunk

--- src/hooks/useUser.js
+++ src/hooks/useUser.js
@@ -1,17 +1,23 @@
 import { useDispatch, useSelector } from "react-redux";
-import { userLoginAction } from "../redux/actions/actionCreators";
+import { userLogoutAction } from "../redux/actions/actionCreators";
+import { userLoginThunk } from "../redux/thunks/userThunks";
 
 function useUser() {
   const user = useSelector(({ user }) => user);
   const dispatch = useDispatch();
 
   const userLogin = (user) => {
-    dispatch(userLoginAction(user));
+    dispatch(userLoginThunk(user));
+  };
+
+  const userLogout = () => {
+    dispatch(userLogoutAction());
   };
 
   return {
     user,
     userLogin,
+    userLogout,
   };
 }
 
