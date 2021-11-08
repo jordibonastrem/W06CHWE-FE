659b09c42ef2132e1fc75b89526c299be3bc7f75
ipice1618 <72157834+ipice1618@users.noreply.github.com>
2021-11-08 19:16:15 +0100

    added login paths

--- src/paths/paths.js
+++ src/paths/paths.js
@@ -1,9 +1,11 @@
-const paths = {
-  get: "/",
-  post: `/create?token=${process.env.REACT_APP_TOKEN}`,
-  getById: "/:idRobot",
-  put: `/update?token=${process.env.REACT_APP_TOKEN}`,
-  delete: `/delete/:idRobot${process.env.REACT_APP_TOKEN}`,
+export const paths = {
+  get: "robots/",
+  post: `robots/create?token=${process.env.REACT_APP_TOKEN}`,
+  getById: "robots/:idRobot",
+  put: `robots/update?token=${process.env.REACT_APP_TOKEN}`,
+  delete: `robots/delete/:idRobot${process.env.REACT_APP_TOKEN}`,
 };
 
-export default paths;
+export const usersPaths = {
+  login: "users/login",
+};
